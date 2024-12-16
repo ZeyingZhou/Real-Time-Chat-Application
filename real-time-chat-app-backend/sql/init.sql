74f87c1a8d5547e246934499dd3f1c7c5c5af2ca
ZeyingZhou <47362333+ZeyingZhou@users.noreply.github.com>
2024-12-17 00:36:29 +0100

    Update init.sql

--- real-time-chat-app-backend/sql/init.sql
+++ real-time-chat-app-backend/sql/init.sql
@@ -1,7 +1,5 @@
 -- Users Table
 CREATE TABLE IF NOT EXISTS users (
--- Users Table
-CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
